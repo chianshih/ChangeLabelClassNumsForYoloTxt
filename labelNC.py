51874975ee769102795d1061d3ff1356a9b2e939
chianshih <chian.shih@gmail.com>
2022-12-22 11:40:09 +0800

    upload main code

---
+++ labelNC.py
@@ -0,0 +1,18 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+def findAllFn(myPath):
+    myList = [f for f in listdir(myPath) if isfile(join(myPath, f))]
+    return myList
+
+targetPath = "SMT/test/labels"
+
+labelList = findAllFn(targetPath)
+
+for txt in labelList:
+    f = open(join(targetPath,txt))
+    lines = f.readlines()
+    fw = open(join(targetPath,txt),"+w")
+    str1 = str(0)+lines[0][2:]
+    fw.write(str1)
